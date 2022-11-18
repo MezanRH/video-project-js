84f5568dd2af40bfe32d3687010aa37e6b25e01b
Mezan <meaznurr@gmail.com>
2022-11-18 21:20:08 +0600

    Alhamdulilah

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container")
+
+btn.addEventListener("click", function() {
+    if(!btn.classList.contains("slide")) {
+        btn.classList.add("slide");
+        video.pause()
+    }else{
+        btn.classList.remove("slide")
+        video.play()
+    }
+});
+
+// preloader
+
+const preload = document.querySelector(".preloader");
+
+window.addEventListener("load", function() {
+    preload.classList.add("hide-preloader");
+})
